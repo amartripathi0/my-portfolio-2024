621b1e22245c95d4f926a4be23c97ceaa7f7c220
amartripathi0 <theamartripathi@gmail.com>
2024-02-18 20:06:36 +0530

    homepage animation added

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,47 +1,65 @@
+"use client"
 import React from "react";
 import Spotlight from "@/components/ui/Spotlight";
 import Image from "next/image";
 import { TextGenerateEffect } from "@/components/ui/TextGenEffect";
-
+import {motion} from 'framer-motion'
 const page = () => {
   return (
     <div >
 
-      <div className="mx-44 gap-6 h-screen flex-between max-tablet:flex max-tablet:flex-col max-tablet:flex-center">
+      <div 
+         
+      className="mx-44 gap-6 h-screen flex-between max-tablet:flex max-tablet:flex-col max-tablet:flex-center">
         {/* left image*/}
 
-        <div className="ml-[1vw] rounded-full   border-1   h-[20vw] w-[20vw] relative -z-10 
+        <motion.div 
+         initial={{ opacity: 0 , scale : 0.7}}
+         animate={{ opacity: 1  , scale : 1}}
+         transition={ { ease : 'easeInOut', duration: 1 , delay:0.4}}
+        className="ml-[1vw] rounded-full   border-1   h-[20vw] w-[20vw] relative -z-10 
           max-tablet:h-40 max-tablet:w-40
         ">
-
           <Image
             src={"/assets/8a06122a-216f-4197-acb3-5bad9770561e_2k-modified.png"}
             alt="Main Profile picture"
             fill
             className="object-cover  "
           />
-        </div>
+        </motion.div>
 
-        <div className="flex flex-col gap-[6vw]">
+        <motion.div className="flex flex-col gap-[6vw]">
           <div className="flex  flex-col text-[10vw]  items-end max-tablet:items-center 
           max-tablet:text-6xl  font-bold">
             
-          <h1 className=" tracking-wider ">AMAR </h1>
-          <h1 className="  opacity-60 max-tablet:m-0 leading-10 max-tablet:leading-none">
+          <motion.h1
+           initial={{ opacity: 0 , y : -30}}
+          animate={{ opacity: 1  , y: 0}}
+          transition={ { ease : 'easeInOut', duration: 1 , delay : 1.5}}
+          className=" tracking-wider ">AMAR </motion.h1>
+          <motion.h1 
+            initial={{ opacity: 0 , y : -30}}
+            animate={{ opacity: 1  , y: 0}}
+            transition={ { ease : 'easeInOut', duration: 1 , delay : 2}}
+          className="  text-[#A9A9A9] max-tablet:m-0 leading-10 max-tablet:leading-none">
             TRIPATHI
-          </h1>
+          </motion.h1>
           </div>
 
-            <div className="text-[1.3vw] text-right max-tablet:text-center max-tablet:text-sm">
-          
+            <motion.div
+            initial={{ opacity: 0 , x : 50}}
+            animate={{ opacity: 1  , x : 0}}
+            transition={ { ease : 'easeInOut', duration: 1.5 , delay : 3}}
+            className="text-[1.3vw] text-right max-tablet:text-center max-tablet:text-sm">
+            
           
           <p> Hey!! 👋, 
-           I'm a <span className="text-purple-300 font-medium hover:text-purple-400 transition-colors duration-500">Full-Stack Web Developer
+           I&apos;m a <span className="text-purple-300 font-medium hover:text-purple-400 transition-colors duration-500">Full-Stack Web Developer
             </span>, turning your ideas into reality.
           </p> 
           <TextGenerateEffect className="text-[1.3vw] text-right max-tablet:text-center max-tablet:text-sm" words="Passionate about Coding 👨‍💻, Music  🎵, Video 🎥 ."/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     
       </div>
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,17 +7,21 @@ import Link from "next/link";
 import Image from "next/image";
 import { AiOutlineMenu } from "react-icons/ai";
 import { IoCloseOutline } from "react-icons/io5";
-import { useRouter ,  } from "next/router";
+import { easeInOut, motion, stagger } from "framer-motion"
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   // const router = useRouter()
   const [menubarOpen , setMenubarOpen] = useState(false)
   const pathname = usePathname();
-  console.log(pathname);
-  
+
+    
   return (
-    <nav 
+    <motion.nav    
+    initial={{ opacity: 0 , y: -15}}
+    animate={{ opacity: 1  , y : 0  }}
+    transition={ { ease : 'easeInOut', duration: 1}}
+
       className="flex justify-between items-center fixed top-0 left-0  
       border-b border-purple-800  w-screen h-28  max-mobile:h-20
     px-8 max-mobile:px-2 py-4 max-mobile:py-2   bg-opacity-30  backdrop-filter backdrop-blur-lg  
@@ -69,22 +73,41 @@ const Navbar = () => {
 
       {
         menubarOpen &&
-        <div className="w-screen h-screen absolute top-0 right-0 bg-black flex-center bg-opacity-85">
+        <motion.div  
+        initial={{ opacity: 1 , x: "100vw" }}
+        animate={{ opacity: 1  , x : 0 }}
+        transition={ { ease : 'easeInOut', duration: 0.3}}
+     className="w-screen h-screen absolute top-0 right-0 backdrop:blur-md bg-black flex-center bg-opacity-85 ">
 
-        <div className="flex-col-center justify-center gap-6 rounded-3xl px-6 ">
+        <div  className="flex-col-center justify-center gap-10 rounded-3xl px-6 ">
+
         <IoCloseOutline 
+        className="absolute top-5 right-5"
         onClick={() => setMenubarOpen(prev => !prev) }
-        size={20} />
+        size={35} />
 
         {navbarItems.map((item) => (
             <Link key={item} href={item} 
-            className={`${pathname === ('/'+item) && 'text-purple-400 font-medium	underline'}`}
-            >{item}</Link>
+            className={`${pathname === ('/'+item) && 'text-purple-400 font-medium	underline'}
+            text-2xl
+            `}
+            >
+              <motion.div
+              initial={{ opacity: 0 , scale : 0.7}}
+              animate={{ opacity: 1  , scale : 1 ,}}
+              transition={ { ease : 'easeInOut', duration: 1 }
+            }
+              >
+
+              {item}
+              </motion.div>
+              
+              </Link>
             ))}
       </div>
-      </div>}
+      </motion.div>}
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
