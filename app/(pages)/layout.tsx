e341252ef7cb40afe5152ab1652f30c4aa68b459
amartripathi0 <theamartripathi@gmail.com>
2024-04-02 18:52:18 +0530

    updated:layout height

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -17,10 +17,10 @@ const Home = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={`relative}`}>
       {locomotiveScroll && <Navbar locomotiveScroll={locomotiveScroll} />}
-      <div className="px-10 mx-40 max-sm:mx-0 max-sm:px-4 pt-28 max-sm:pt-10">
+      <div className="px-10 mx-40 max-sm:mx-0 max-sm:px-4 pt-28 max-sm:pt-20">
         {children}
       </div>
-        <div className="mr-10 bottom-4 left-full sticky border p-3  flex-center w-60 max-sm:w-full text-sm max-sm:m-auto max-sm:p-1 max-sm:bottom-1 max-sm:left-0 rounded-xl  ">
+        <div className="mr-10 bottom-4 left-full sticky border p-3 flex-center w-60 max-sm:w-full text-sm max-sm:m-auto max-sm:p-1 max-sm:bottom-1 max-sm:left-0 rounded-xl  ">
           Website is currently under construction. Please check back soon!
         </div>
     </div>
