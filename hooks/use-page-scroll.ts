f5cce21f61bebe5785b4f8c4f13c047e19673de1
amartripathi0 <theamartripathi@gmail.com>
2024-04-02 14:17:21 +0530

    added:custom scroll bar

--- hooks/use-page-scroll.ts
+++ hooks/use-page-scroll.ts
@@ -1,11 +1,40 @@
-"use client"
-import { useEffect, useState } from 'react'
+"use client";
+import { useEffect, useState } from "react";
 function usePageScroll() {
   const [pageSectionOnViewport, setPageSectionOnViewport] = useState("home");
   const [windowHeight, setWindowHeight] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
+
   useEffect(() => {
-    setWindowHeight(window.innerHeight)
+    if (isScrolling) {
+      if (!document.body.classList.contains("on-scroll")) {
+        document.body.classList.add("on-scroll");
+        document.body.classList.remove("on-not-scroll");
+      }
+    } else {
+      if (!document.body.classList.contains("on-not-scroll")) {
+        document.body.classList.add("on-not-scroll");
+        document.body.classList.remove("on-scroll");
+      }
+    }
+  }, [isScrolling]);
+  useEffect(() => {
+    setWindowHeight(window.innerHeight);
     function handlePageScroll() {
+      handlePageNavigation();
+      handleScrollbar();
+    }
+
+    function handleScrollbar() {
+      handleScrolling();
+    }
+    let scrollTimer: NodeJS.Timeout;
+    function handleScrolling() {
+      setIsScrolling(true);
+      clearInterval(scrollTimer);
+      scrollTimer = setTimeout(() => setIsScrolling(false), 100);
+    }
+    function handlePageNavigation() {
       if (window.scrollY <= windowHeight) {
         setPageSectionOnViewport("home");
       } else if (
@@ -25,11 +54,11 @@ function usePageScroll() {
         setPageSectionOnViewport("projects");
       }
     }
-      window.addEventListener('scroll' , () => handlePageScroll());
-      return  window.removeEventListener('scroll', () => handlePageScroll());
-  } , [ windowHeight])
-  
-return {pageSectionOnViewport};
+    window.addEventListener("scroll", (e) => handlePageScroll());
+    return window.removeEventListener("scroll", (e) => handlePageScroll());
+  }, [windowHeight]);
+
+  return { pageSectionOnViewport };
 }
 
-export default usePageScroll
+export default usePageScroll;
