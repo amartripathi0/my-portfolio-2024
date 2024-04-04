747e615608a87032187013fc493d9633d2d3b007
amartripathi0 <theamartripathi@gmail.com>
2024-04-04 23:40:47 +0530

    updated:projects layout

--- app/(pages)/_projects/page.tsx
+++ app/(pages)/_projects/page.tsx
@@ -4,7 +4,11 @@ import TextUnderline from "@/components/shared/TextUnderline";
 
 function Projects() {
   return (
-    <div id="projects" className="min-h-screen border-red-500 border-2" data-scroll-container>
+    <div
+      id="projects"
+      className="min-h-screen border-red-500 border-2"
+      data-scroll-container
+    >
       <div id="containerA" className="top-0 pt-20 shadow-md">
         <TextUnderline
           text="My Projects"
@@ -12,9 +16,18 @@ function Projects() {
           containerDivStyles="w-1/4 max-sm:w-full  max-sm:flex-center sticky top-32"
           underlineStyles=" bg-gradient-to-r from-indigo-600 via-purple-400 to-cyan-600"
         />
-        <ProjectContainer projectName="Project 1" />
-        <ProjectContainer projectName="Project 2" />
-        <ProjectContainer projectName="Project 3" />
+        <ProjectContainer
+          projectTitle="Placement Nexus"
+          projectBio="Placement Nexus is an ongoing MERN stack-based project
+          streamlining college hiring. It connects students, recruiters, and
+          colleges for efficient recruitment. Stay tuned for major frontend
+          and backend enhancements."
+          projectThumbnailSrc="/assets/placement-nexus.png"
+          projectGithubLink="https://github.com/amartripathi0/Web_Placement_Portal"
+          projectDeployedLink="https://placement-nexus.vercel.app/"
+          projectDetail=" The College Placement Portal isn't just a static platform; it's an ongoing endeavor aimed at simplifying and managing the dynamic process of job placements for students, colleges, and companies. Serving as a digital nexus, it fosters seamless communication and interaction among these stakeholders, ensuring a smooth and transparent journey through placements.Notably  ongoing major updates to both the frontend and backend ensure that the portal remains at the forefront of innovation, continuously evolving to meet the changing needs of its users."
+        />
+
       </div>
     </div>
   );
---
+++ components/ProjectCard.tsx
@@ -0,0 +1,65 @@
+import {
+  CardBody,
+  CardContainer,
+  CardItem,
+} from "@/components/ui/3d-container";
+
+import Image from "next/image";
+import Link from "next/link";
+function ProjectCard() {
+  return (
+    <CardContainer
+      data-scroll-sticky
+      containerClassName="sticky top-40   "
+      className=""
+    >
+      <CardBody className="bg-gray-800 relative group/card h-full rounded-xl p-6 border  ">
+        <CardItem translateZ="50" className="text-xl font-bold text-white">
+          Placement Nexus
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-white text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Placement Nexus is an ongoing MERN stack-based project streamlining
+          college hiring. It connects students, recruiters, and colleges for
+          efficient recruitment. Stay tuned for major frontend and backend
+          enhancements.{" "}
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src="/assets/placement-nexus.png"
+            height="1000"
+            width="1000"
+            className="h-3/5 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://github.com/amartripathi0/Web_Placement_Portal"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Source Code
+          </CardItem>
+
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://placement-nexus.vercel.app/"
+            target="__blank"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            See it live →
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
+}
+
+export default ProjectCard;
