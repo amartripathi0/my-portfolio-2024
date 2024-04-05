dc38af7e1a646d7f81187fc53a38b03c44744a66
amartripathi0 <theamartripathi@gmail.com>
2024-04-05 15:55:54 +0530

    updated:global sticky message

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -22,7 +22,7 @@ const Home = ({ children }: { children: React.ReactNode }) => {
         {children}
       </div>
         <div className="mr-10 bottom-4 left-full sticky border p-3 flex-center w-60 max-sm:w-full text-sm max-sm:m-auto max-sm:p-1 max-sm:bottom-1 max-sm:left-0 rounded-xl  ">
-          Website is currently under construction. Please check back soon!
+        I&apos;m updating the website. Please visit often for the latest changes!
         </div>
     </div>
   );
