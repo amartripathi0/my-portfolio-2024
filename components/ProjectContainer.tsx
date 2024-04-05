528b317125dbe32c5ae9fad3ce00af366e9c878d
amartripathi0 <theamartripathi@gmail.com>
2024-04-05 14:13:59 +0530

    updated:placement nexus project

--- components/ProjectContainer.tsx
+++ components/ProjectContainer.tsx
@@ -6,73 +6,107 @@ import {
 import { ProjectContainerProps } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import { Badge } from "./ui/badge";
+import TextUnderline from "./shared/TextUnderline";
 
- const ProjectContainer = ({ projectDetail ,  projectTitle , projectBio , projectThumbnailSrc, projectGithubLink , projectDeployedLink ,}: ProjectContainerProps) => {
-    return (
-      <div className="flex justify-between min-h-[calc(100vh-7rem)] w-full border-l ">
+const ProjectContainer = ({
+  projectDetail,
+  projectTitle,
+  projectBio,
+  projectThumbnailSrc,
+  projectGithubLink,
+  projectDeployedLink,
+  projectTools,
+}: ProjectContainerProps) => {
+  return (
+    <div className="flex justify-between min-h-[calc(100vh-7rem)] w-full border-l border-purple-600 ">
       {/* Sticky Image */}
-      <div className="w-[30%] sticky  ">
-      <div
-        className="absolute opacity-80 top-1/4 left-10 h-1/2  aspect-square  blur-[100px] rounded-full
-        bg-gradient-to-r from-violet-600 to-indigo-700  
-      "
-      ></div>
+      <div className="w-[30%] sticky top-40  ">
+        <IndigoVioletBlur />
 
-        <CardContainer        
-        data-scroll-sticky	
-         containerClassName="sticky top-20 left-1/2  "
-         className=""
-       >
-         <CardBody className="bg-slate-900 relative group/card h-full rounded-xl p-6 border  ">
-           <CardItem translateZ="50" className="text-3xl font-bold text-white">
-             {projectTitle}
-           </CardItem>
-           <CardItem
-             as="p"
-             translateZ="60"
-             className="text-white text-md max-w-sm mt-2 dark:text-neutral-300"
-           >
-             {projectBio}
-           </CardItem>
-           <CardItem translateZ="100" className="w-full mt-4">
-             <Image
-               src={projectThumbnailSrc}
-               height="1000"
-               width="1000"
-               className="h-3/5 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-               alt="thumbnail"
-             />
-           </CardItem>
-           <div className="flex justify-between items-center mt-20">
-             <CardItem
-               translateZ={20}
-               as={Link}
-               href={projectGithubLink}
-               target="__blank"
-               className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-             >
-               Source Code
-             </CardItem>
+        <CardContainer
+          data-scroll-sticky
+          containerClassName="sticky top-40 left-1/2  "
+          className=""
+        >
+          <CardBody className="bg-slate-900 relative group/card h-full rounded-xl p-6 border  ">
+            <CardItem translateZ="50" className="text-3xl font-bold text-white">
+              {projectTitle}
+            </CardItem>
+            <CardItem
+              as="p"
+              translateZ="60"
+              className="text-white text-md max-w-sm mt-2 dark:text-neutral-300"
+            >
+              {projectBio}
+            </CardItem>
+            <CardItem translateZ="100" className="w-full mt-4">
+              <Image
+                src={projectThumbnailSrc}
+                height="1000"
+                width="1000"
+                className="h-3/5 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                alt="thumbnail"
+              />
+            </CardItem>
+            <div className="flex justify-between items-center mt-20">
+              <CardItem
+                translateZ={20}
+                as={Link}
+                href={projectGithubLink}
+                target="__blank"
+                className="px-4 py-2 rounded-xl text-sm font-normal dark:text-white"
+              >
+                Source Code
+              </CardItem>
 
-             <CardItem
-               translateZ={20}
-               as={Link}
-               href={projectDeployedLink}
-               target="__blank"
-               className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-             >
-               See it live →
-             </CardItem>
-           </div>
-         </CardBody>
-       </CardContainer>
+              <CardItem
+                translateZ={20}
+                as={Link}
+                href={projectDeployedLink}
+                target="__blank"
+                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-sm font-bold"
+              >
+                See it live →
+              </CardItem>
+            </div>
+          </CardBody>
+        </CardContainer>
       </div>
 
       {/* Project Details */}
       <div className="w-[70%] flex flex-col  ">
-        <div className=" p-10 pt-20 rounded-xl shadow-md h-full text-lg text-justify">{projectDetail} /</div>
+        <div className="flex flex-col gap-6 p-10 pt-20 pr-0 rounded-xl shadow-md h-full text-lg text-justify">
+          <p>{projectDetail.topPara}</p>
+
+          <div className="flex gap-6 flex-wrap">
+            {projectTools.map((eachTool) => (
+              <Badge
+                className=" text-sm p-2 px-4 bg-prelude-900"
+                key={eachTool}
+              >
+                <TextUnderline text={eachTool} />
+              </Badge>
+            ))}
+          </div>
+          <div className="flex flex-col gap-6">
+            {projectDetail.parasArray?.map((projectDetailPara) => (
+              <p key={projectDetailPara}>{projectDetailPara}</p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-    );
-  };
+  );
+};
 export default ProjectContainer;
+
+function IndigoVioletBlur() {
+  return (
+    <div
+      className="absolute opacity-50 h-2/3 top-1/4  w-1/5 blur-[100px]   rounded-full
+        bg-gradient-to-r from-violet-600 to-indigo-700  
+      "
+    />
+  );
+}
--- constants/index.ts
+++ constants/index.ts
@@ -22,36 +22,78 @@ export const socialMediaLinks: {
     icon: FaLinkedin,
   },
   { label: "GitHub", url: "https://github.com/amartripathi0", icon: FaGithub },
-  { label: "Twitter", url: "https://twitter.com/amartripathi_", icon: FaTwitter },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/amartripathi_",
+    icon: FaTwitter,
+  },
 ];
 
-export const resumeLink : string = "https://drive.google.com/file/d/1pzhRz_U4GPml2qQMDWewn6uE0Ugl9N5K/view"
+export const resumeLink: string =
+  "https://drive.google.com/file/d/1pzhRz_U4GPml2qQMDWewn6uE0Ugl9N5K/view";
 
 export type ProjectContainerProps = {
-  projectDetail : string ;
-  projectTitle : string;
-  projectBio : string;
-  projectGithubLink : string;
-  projectDeployedLink ?: string;
-  projectThumbnailSrc : string;
-  
-}
-export const projectsArray :ProjectContainerProps[] = [
-        {
-          projectTitle : "Placement Nexus",
-          projectBio : "Placement Nexus is an ongoing MERN stack-based projectstreamlining college hiring. It connects students, recruiters, andcolleges for efficient recruitment. Stay tuned for major frontendand backend enhancements.",
-          projectThumbnailSrc : "/assets/placement-nexus.png",
-          projectGithubLink : "https://github.com/amartripathi0/Web_Placement_Portal",
-          projectDeployedLink : "https://placement-nexus.vercel.app/",
-          projectDetail : " The College Placement Portal isn't just a static platform; it's an ongoing endeavor aimed at simplifying and managing the dynamic process of job placements for students, colleges, and companies. Serving as a digital nexus, it fosters seamless communication and interaction among these stakeholders, ensuring a smooth and transparent journey through placements.Notably  ongoing major updates to both the frontend and backend ensure that the portal remains at the forefront of innovation, continuously evolving to meet the changing needs of its users."
-        },
-        {
-          projectTitle : 'Youtube Playlist Analyzer',
-          projectDetail : "A simple project to analyze the time required to watch a youtube playlist in various available speed." ,
-          projectBio : "A simple project to analyze the time required to watch a youtube playlist in various available speed." ,
-          projectGithubLink : "https://github.com/amartripathi0/youtube-playlist-analyzer",
-          projectDeployedLink : "https://youtube-playlist-analyzer-teal.vercel.app/",
-          projectThumbnailSrc : '/assets/yt-playlist-analyzer.png',
-        },
-       
-]
+  projectDetail: {
+    topPara: string;
+    parasArray?: string[];
+  };
+  projectTitle: string;
+  projectBio: string;
+  projectGithubLink: string;
+  projectTools: string[];
+  projectDeployedLink?: string;
+  projectThumbnailSrc: string;
+};
+export const projectsArray: ProjectContainerProps[] = [
+  {
+    projectTitle: "Placement Nexus",
+    projectBio:
+      "Placement Nexus is an ongoing MERN stack-based projectstreamlining college hiring. It connects students, recruiters, andcolleges for efficient recruitment. Stay tuned for major frontendand backend enhancements.",
+    projectThumbnailSrc: "/assets/placement-nexus.png",
+    projectGithubLink: "https://github.com/amartripathi0/Web_Placement_Portal",
+    projectDeployedLink: "https://placement-nexus.vercel.app/",
+    projectDetail: {
+      topPara:
+        "'Placement Nexus' stands as a pivotal Full Stack MERN project I developed for my university minor project. The primary aim behind creating this platform was to delve deep into the intricacies of full-stack web development. I found myself intrigued by fundamental questions, such as the necessity of routing in React and then again on the backend, and sought to understand the nuances of how the frontend and backend communicate.",
+      parasArray: [
+        "Through this project, I unraveled the efficiencies of React and its component-based architecture, along with the styling capabilities of Tailwind CSS. React Hook Form was a revelation, teaching me about client-side form validation that significantly reduces page re-renders compared to traditional validation methods.",
+        "Moreover, the project was a deep dive into Authentication and Authorization, employing libraries such as bcrypt, JWT, and cookies. The concept of hashing passwords before storing them in the database was fascinating, illustrating a level of security and privacy that ensures even developers cannot access user passwords. This project was not just a technical journey but an enlightening experience that broadened my understanding and skills in full-stack web development.",
+        "To truly embrace the cloud, deployed the frontend on Vercel and the backend on Cyclic.sh, with the database hosted on MongoDB Atlas cloud. This approach allowed me to experience the seamless integration and advantages of cloud-based development.",
+        "This project is an ongoing adventure, with major parts still under development. I invite you to keep an eye on this project as it evolves. For a glimpse into its current state and to follow its progress, click on the live deployed link and visit the GitHub repository for the source code. This journey has been more than just a technical endeavor; it has been a profound learning experience that has expanded my knowledge and skill set in full-stack web development."
+      ],
+    },
+    projectTools: [
+      "React",
+      "React Hook Form",
+      "React Router DOM",
+      "Redux Toolkit",
+      "Axios",
+      "React Toastify",
+      "TailwindCSS",
+      "NodeJS",
+      "ExpressJs",
+      "MongoDB Atlas",
+      "Mongoose",
+      "Multer",
+      "Postman",
+      "bcrypt",
+      "jsonwebtoken",
+      "Vercel",
+      "Cyclic.sh",
+    ],
+  },
+  {
+    projectTitle: "Youtube Playlist Analyzer",
+    projectDetail: {
+      topPara:
+        "A simple project to analyze the time required to watch a youtube playlist in various available speed.",
+    },
+    projectBio:
+      "A simple project to analyze the time required to watch a youtube playlist in various available speed.",
+    projectGithubLink:
+      "https://github.com/amartripathi0/youtube-playlist-analyzer",
+    projectDeployedLink: "https://youtube-playlist-analyzer-teal.vercel.app/",
+    projectThumbnailSrc: "/assets/yt-playlist-analyzer.png",
+    projectTools: [],
+  },
+];
