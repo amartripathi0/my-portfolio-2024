4cfecdf3df763951212f4d34f5fd13e347176b5a
amartripathi0 <theamartripathi@gmail.com>
2024-04-05 14:13:59 +0530

    updated:placement nexus project

--- app/(pages)/_projects/page.tsx
+++ app/(pages)/_projects/page.tsx
@@ -5,48 +5,52 @@ import { projectsArray } from "@/constants";
 
 function Projects() {
   return (
-    <div
-      id="projects"
-      className="min-h-screen "
-      data-scroll-container
-    >
-      <div className="top-0 pt-20 shadow-md relative  ">
-
+    <div id="projects" className="min-h-screen " data-scroll-container>
+      <div className="pt-10 shadow-md relative">
         <TextUnderline
           text="My Projects"
           textStyles="text-[3.2vw] max-sm:text-4xl font-medium "
-          containerDivStyles="w-1/4 max-sm:w-full  max-sm:flex-center"
+          containerDivStyles="w-1/4 max-sm:w-full  max-sm:flex-center "
           underlineStyles=" bg-gradient-to-r from-indigo-600 via-purple-400 to-cyan-600"
         />
-         {/* bg gradient indigo cyan */}
-      <div
-        className="absolute opacity-80 top-0 left-10 h-60  aspect-square  blur-[100px] rounded-full
-        bg-gradient-to-r from-blue-600 to-violet-600
-      " 
-      ></div>
+        {/* bg gradient Violet cyan */}
+          <CyanVioletBlur />
 
-        {
-          projectsArray.map(({projectTitle,
+        {projectsArray.map(
+          ({
+            projectTitle,
             projectBio,
             projectThumbnailSrc,
             projectGithubLink,
             projectDeployedLink,
-            projectDetail,}) => (
+            projectDetail,
+            projectTools
+          }) => (
             <ProjectContainer
               key={projectTitle}
-              projectTitle = {projectTitle}
-              projectBio = {projectBio}
-              projectThumbnailSrc = {projectThumbnailSrc}
-              projectGithubLink = {projectGithubLink}
-              projectDeployedLink = {projectDeployedLink}
-              projectDetail = {projectDetail}
+              projectTitle={projectTitle}
+              projectBio={projectBio}
+              projectTools={projectTools}
+              projectThumbnailSrc={projectThumbnailSrc}
+              projectGithubLink={projectGithubLink}
+              projectDeployedLink={projectDeployedLink}
+              projectDetail={projectDetail}
             />
-          ))
-        }
-
+          )
+        )}
       </div>
     </div>
   );
 }
 
 export default Projects;
+
+const CyanVioletBlur = () => {
+  return (
+    <div
+      className="absolute opacity-80 top-20  h-20  w-96  -z-10 blur-[100px] rounded-full
+        bg-gradient-to-r from-cyan-400 to-violet-600
+      "
+    />
+  );
+};
