4ae3e53ecccae75fcb8bb44a9e8c3fcb733ec86b
amartripathi0 <theamartripathi@gmail.com>
2024-04-07 10:13:45 +0530

    updated:blur bg for about me page

--- components/shared/IndigoBlurBackground.tsx
+++ components/shared/IndigoBlurBackground.tsx
@@ -1,7 +1,7 @@
-function IndigoBlurBackground({top , left , height ,right , minusTop } : {top ?: string , left ?: string , height ?: string , right ?: string , minusTop ?: string}) {
+function IndigoBlurBackground({top , left , height ,right , minusTop , additionalStyles } : {top ?: string , left ?: string , height ?: string , right ?: string , minusTop ?: string , additionalStyles ?: string}) {
   return (
     < div
-    className={`absolute opacity-80 left-${left} h-${height} top-${top} -top-${minusTop} right-${right} aspect-square blur-[100px]  rounded-full
+    className={`${additionalStyles} absolute opacity-80 left-${left} h-${height} top-${top} -top-${minusTop} right-${right} aspect-square blur-[100px]  rounded-full
     bg-gradient-to-r from-indigo-600  to-cyan-400  
     `}
     />  )
