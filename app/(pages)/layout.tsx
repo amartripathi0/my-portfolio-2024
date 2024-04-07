1a7853a6b0590ecb957fdcc26052303495095921
amartripathi0 <theamartripathi@gmail.com>
2024-04-07 13:40:09 +0530

    added: projects page responsiveness

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -16,14 +16,14 @@ const Home = ({ children }: { children: React.ReactNode }) => {
   }, []);
 
   return (
-    <div className={`relative}`}>
+    <div className={`relative} max-sm:overflow-x-hidden`}>
       {locomotiveScroll && <Navbar locomotiveScroll={locomotiveScroll} />}
       <div className="px-10 mx-40 max-sm:mx-0 max-sm:px-4 pt-28 max-sm:pt-20">
         {children}
       </div>
-        <div className="mr-10 bottom-4 left-full sticky border p-3 flex-center w-60 max-sm:w-full text-sm max-sm:m-auto max-sm:p-1 max-sm:bottom-1 max-sm:left-0 rounded-xl  ">
+        {/* <div className="mr-10 bottom-4 left-full sticky border p-3 flex-center w-60 max-sm:w-full text-sm max-sm:m-auto max-sm:p-1 max-sm:bottom-1 max-sm:left-0 rounded-xl  ">
         I&apos;m updating the website. Please visit often for the latest changes!
-        </div>
+        </div> */}
     </div>
   );
 };
