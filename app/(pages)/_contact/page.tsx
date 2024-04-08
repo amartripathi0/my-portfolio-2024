1f27cd5ead8ac3b689b99ec95cba4e50d3473fb5
amartripathi0 <theamartripathi@gmail.com>
2024-04-08 23:46:41 +0530

    added:temp social links in contact me page

--- app/(pages)/_contact/page.tsx
+++ app/(pages)/_contact/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import SocialHandles from "@/components/shared/SocialHandles";
 import TextUnderline from "@/components/shared/TextUnderline";
 import { underlineAnimationVariant } from "@/constants";
 import { motion } from "framer-motion";
+import { IoMail } from "react-icons/io5";
 
 function Contact() {
   return (
@@ -29,9 +31,16 @@ function Contact() {
       ></div>
 
       <div className="text-2xl max-sm:text-sm max-sm:px-2 text-justify  flex-col flex-center gap-4">
+        <h2 className="mb-20">
+          {"// "}Contact Me page is yet to be implemented 👨‍💻."     </h2>
         <h2>
-          {"// "}Contact Me page is yet to be implemented. {";)"}
-        </h2>
+         Let&apos;s connect! I&apos;ll get back to you within 24 hours.</h2>
+
+          <div>
+          <SocialHandles additionalStyle="flex-center gap-6 max-sm:gap-2 rounded-xl  p-4 max-sm:w-40 " />
+
+          </div>
+          <div className="flex-center gap-4"> <IoMail size={22}/>  theamartripathi@gmail.com </div>
       </div>
     </div>
   );
