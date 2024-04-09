2625324b68b16a5b985b2ff301dfd85bd40be055
amartripathi0 <theamartripathi@gmail.com>
2024-04-09 09:08:15 +0530

    fixed-scale:navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,14 +26,14 @@ const Navbar = ({
       initial={{ opacity: 0, y: -15 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ ease: "easeInOut", duration: 1 }}
-      className={`flex-between fixed top-0 left-0 w-screen h-28  max-mobile:h-20
-    px-10 max-mobile:px-4 py-4 max-mobile:py-2 backdrop-blur-md z-50 ${
+      className={`flex-between fixed top-0 left-0 w-screen h-20   max-mobile:h-20
+    px-10 max-mobile:px-4 py-2 max-mobile:py-2 backdrop-blur-md z-50${
       scrolled && "border-b border-violet-700"
     }
     `}
     >
       {/* Left: Avatar and Name section */}
-      <WrapperContainer additionalStyle="flex-center  gap-4 px-4  w-40 max-mobile:w-auto max-sm:px-1">
+      <WrapperContainer additionalStyle="flex-center  w-28 max-mobile:w-auto px-1">
         <Link href={"/"} className="flex-around-center w-full  ">
           <Avatar>
             <AvatarImage src={"/assets/profile-photo.jpeg"} alt="profile-pic" />
@@ -44,7 +44,7 @@ const Navbar = ({
       </WrapperContainer>
 
       {/* Middle */}
-      <WrapperContainer additionalStyle="flex-center gap-8 rounded-full px-3  max-tablet:hidden w-1/3">
+      <WrapperContainer additionalStyle="flex-center gap-8 rounded-full px-2  max-tablet:hidden w-1/3">
         {navbarItems.map((item) => (
           <Link
             key={item.label}
@@ -80,7 +80,7 @@ const Navbar = ({
             href={"#contact-me"}
             onClick={() => locomotiveScroll.scrollTo("#contact-me", {offset : 0} )}
 
-            className={`${pageSectionOnViewport === 'contact-me' && "shadow-md shadow-violet-500 rounded-full"} font-medium flex-center  w-40 py-4 max-tablet:py-3 max-sm:w-32 max-sm:px-2 px-3`}
+            className={`${pageSectionOnViewport === 'contact-me' && "shadow-md shadow-violet-500 rounded-full"} font-medium flex-center  w-28 py-2 max-tablet:py-3 max-sm:w-32 max-sm:px-2 `}
           >
             <TextUnderline text="Contact Me"/>
           </Link>
