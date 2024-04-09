cee703501768d8fa8af24cc96b9394e9ebce4667
amartripathi0 <theamartripathi@gmail.com>
2024-04-09 23:43:43 +0530

    fixed:about me page scale

--- app/(pages)/_about/page.tsx
+++ app/(pages)/_about/page.tsx
@@ -15,10 +15,10 @@ const   About = () => {
   return (
     <div
       id="about-me"
-      className="h-[calc(100vh-7rem)] max-sm:h-[calc(100vh-5rem)] max-sm:pt-0 flex-between relative  max-sm:flex-col"
+      className="h-[calc(100vh-5rem)] max-sm:h-[calc(100vh-5rem)] max-sm:pt-0 flex-between relative  max-sm:flex-col"
     >
       {/* Left section */}
-      <div className="flex flex-col w-3/4 sm:h-4/5 max-sm:w-full gap-10 max-sm:gap-2">
+      <div className="flex flex-col w-3/4 sm:h-4/5 max-sm:w-full max-sm:gap-2">
         {/* bg gradient indigo cyan */}
         <IndigoBlurBackground height="1/5" additionalStyles="max-sm:left-1/4" />
         {/* About Me heading */}
@@ -28,8 +28,9 @@ const   About = () => {
           variants={underlineAnimationVariant}
         >
           <TextUnderline
+            isHeading = {true}
             text="About Me"
-            textStyles="text-[3.2vw] max-sm:text-4xl font-medium "
+            textStyles=" max-sm:text-4xl font-medium "
             containerDivStyles="w-[29%] max-sm:w-full  max-sm:flex-center"
             underlineStyles=" bg-gradient-to-r from-indigo-600 via-purple-400 to-cyan-600"
           />
@@ -41,7 +42,7 @@ const   About = () => {
           whileInView={"inView"}
           variants={fadeAnimationVariant}
           viewport={{ once: true }}
-          className="text-xl p-4 w-full max-sm:text-sm max-sm:px-2 text-justify  flex-col flex-center gap-4"
+          className="text-base p-4 w-full max-sm:text-sm max-sm:px-2 text-justify  flex-col flex-center gap-4"
         >
           <p>
             I&apos;m a third-year Computer Science Engineering student from
@@ -106,7 +107,7 @@ const   About = () => {
         whileInView={"inView"}
         variants={fadeAnimationVariant}
         viewport={{ once: true }}
-        className="h-4/5 rounded-4xl w-1/3 flex-center flex-col gap-10 relative  
+        className="h-4/5 rounded-4xl w-1/3 flex-center flex-col gap-6 relative  
           max-tablet:h-40 max-tablet:w-40 flex-center max-sm:hidden
         "
       >
@@ -117,7 +118,7 @@ const   About = () => {
           alt="Main Profile picture"
           height={1000}
           width={1000}
-          className="object-cover h-3/5 w-3/5 aspect-auto rounded-3xl z-10  backdrop:blur-sm opacity-80 max-sm:hidden"
+          className="object-cover h-1/2 w-1/2 aspect-auto rounded-3xl z-10  backdrop:blur-sm opacity-80 max-sm:hidden"
         />
 
         <motion.div
@@ -135,9 +136,9 @@ const   About = () => {
           }}
         >
           <Link href={resumeLink} target="_blank">
-            <Button className="relative inline-flex h-16 w-40 max-sm:h-12 max-sm:w-32 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <Button className="relative inline-flex h-12 w-32 max-sm:h-12 max-sm:w-32 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-              <span className="flex justify-around  h-full w-full cursor-pointer items-center  rounded-full bg-slate-950 px-3 py-1 text-lg max-sm:text-base font-medium text-white backdrop-blur-3xl">
+              <span className="flex justify-around  h-full w-full cursor-pointer items-center  rounded-full bg-slate-950 px-3 py-1 text-md max-sm:text-base font-medium text-white backdrop-blur-3xl">
                 Resume <FaArrowCircleRight size={16} />{" "}
               </span>
             </Button>
