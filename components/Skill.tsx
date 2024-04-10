fa628f1e1023d24e86a0fc8789015ec1fe3f8bff
amartripathi0 <theamartripathi@gmail.com>
2024-04-10 14:06:28 +0530

    fixed:scale of skill container

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -8,7 +8,7 @@ function Skill({ name, imageSrc }: { name: string; imageSrc: string }) {
   return (
     <BackgroundGradient>
 
-    <div className="h-44 max-sm:h-16 aspect-square p-4 max-sm:p-2 max-sm:flex-center flex-between flex-col bg-zinc-800 rounded-3xl ">
+    <div className="h-32 max-sm:h-16 aspect-square max-sm:p-2 p-2  max-sm:flex-center flex-between flex-col bg-zinc-800 rounded-3xl ">
       <Image
         src={imageSrc}
         alt={name}
@@ -16,8 +16,8 @@ function Skill({ name, imageSrc }: { name: string; imageSrc: string }) {
         height={1000}
         className="h-2/3 max-sm:h-full object-contain aspect square rounded-2xl"
       />
-      <Badge className="text-xs max-sm:hidden p-2 px-2 w-full flex-center bg-prelude-900" key={name}>
-        <TextUnderline text={name} />
+      <Badge className="max-sm:hidden h-6  w-full flex-center bg-prelude-900" >
+        <TextUnderline text={name} isSkill = {true} />
       </Badge>
     </div>
     </BackgroundGradient>
@@ -25,3 +25,4 @@ function Skill({ name, imageSrc }: { name: string; imageSrc: string }) {
 }
 
 export default Skill;
+
