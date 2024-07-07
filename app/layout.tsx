63a4b239625a3b2a3281064111efdd3b86827658
amartripathi0 <theamartripathi@gmail.com>
2024-07-08 00:02:19 +0530

    feat: added vercel Analytics and SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'sonner'
-
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -26,6 +27,8 @@ export default function RootLayout({
       <body className={montserrat.className}>
         {children}
         <Toaster richColors closeButton theme="dark" />
+        <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
