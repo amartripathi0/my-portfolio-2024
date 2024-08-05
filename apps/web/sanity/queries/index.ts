6afcb3f626011a782b31bb88373656364b5a184b
amartripathi0 <theamartripathi@gmail.com>
2024-08-05 23:13:30 +0530

    feat: add About page query

--- apps/web/sanity/queries/index.ts
+++ apps/web/sanity/queries/index.ts
@@ -5,3 +5,11 @@ export const HOMEPAGE_QUERY = `
     userDescription,
     heroImage,
 }[0]`;
+
+export const ABOUT_PAGE_QUERY = `
+  *[_type == 'about']{
+    title,
+    resume,
+    aboutContent,
+    heroImage,
+}[0]`;
