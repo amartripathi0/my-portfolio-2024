4348bfab707090f67fd2340dd54e98562a354d60
amartripathi0 <theamartripathi@gmail.com>
2024-08-05 23:14:34 +0530

    fix: landing page chore fix

--- apps/web/app/(pages)/_landing/page.tsx
+++ apps/web/app/(pages)/_landing/page.tsx
@@ -4,20 +4,20 @@ import HomePageLeft from '@/components/HomePageLeft'
 import HomePageRight from '@/components/HomePageRight'
 import { HomepageDataType } from '@/types'
 
-const Landing =  async () => {
-  const {firstName, lastName, heroImage}: HomepageDataType = await getSanityData(HOMEPAGE_QUERY)
+const Landing = async () => {
+  const { firstName, lastName, heroImage }: HomepageDataType = await getSanityData(HOMEPAGE_QUERY)
   return (
     <div
       id="home"
       className="h-[calc(100vh-5rem)] flex-between  max-tablet:flex-col max-sm:justify-start  max-sm:pt-10 "
     >
-    <HomePageLeft 
-      heroImage={heroImage}
-    />
-    <HomePageRight 
-      firstName={firstName}
-      lastName={lastName}
-    />
+      <HomePageLeft
+        heroImage={heroImage}
+      />
+      <HomePageRight
+        firstName={firstName}
+        lastName={lastName}
+      />
     </div>
   )
 }
