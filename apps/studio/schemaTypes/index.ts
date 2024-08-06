0ba6522cdc95031bb5fee38daeaa2f0ca2206871
amartripathi0 <theamartripathi@gmail.com>
2024-08-06 11:15:53 +0530

    feat: add various types in index

--- apps/studio/schemaTypes/index.ts
+++ apps/studio/schemaTypes/index.ts
@@ -1,4 +1,8 @@
-import { aboutPageType } from "./aboutPageType";
-import { homepageType } from "./homepageType";
+import { aboutPageType } from "./blocks/aboutPageType";
+import { homepageType } from "./blocks/homepageType";
+import { projectsPageType } from "./blocks/projectsType";
+import { skillsPageType } from "./blocks/skillsPage";
+import { projectType } from "./documents/projectType";
+import { skillType } from "./documents/skillType";
 
-export const schemaTypes = [homepageType, aboutPageType]
+export const schemaTypes = [homepageType, aboutPageType, projectsPageType , projectType , skillType, skillsPageType]
