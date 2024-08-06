ea8faa122c9e647c9e403732b97c9fd6f23f5060
amartripathi0 <theamartripathi@gmail.com>
2024-08-06 11:14:48 +0530

    feat: add skill type and page

---
+++ apps/studio/schemaTypes/documents/skillType.ts
@@ -0,0 +1,21 @@
+import { defineField, defineType } from "sanity";
+
+export const skillType = defineType({
+    name: 'skill',
+    title: 'Skill',
+    type: 'document',
+
+    fields: [
+        defineField({
+            name: 'skill',
+            title: 'Skill Name',
+            type: 'string',
+        }),
+        defineField({
+            name: 'image',
+            title: 'Skill Image',
+            type: 'image', 
+
+        }),        
+    ]
+})
