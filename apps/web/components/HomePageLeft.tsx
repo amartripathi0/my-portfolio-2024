666953731e07367e277b78ade77ee5ba122a65a3
amartripathi0 <theamartripathi@gmail.com>
2024-08-06 13:22:36 +0530

    fix: mobile first styling in homepage

--- apps/web/components/HomePageLeft.tsx
+++ apps/web/components/HomePageLeft.tsx
@@ -5,23 +5,20 @@ import React from 'react'
 import SocialHandles from './shared/SocialHandles'
 import { urlFor } from '@/utils/urlFor'
 
-function HomePageLeft({heroImage}: {heroImage : any}) {
+function HomePageLeft({ heroImage }: { heroImage: any }) {
   return (
     <motion.div
-      className=" flex-between flex-col max-sm:justify-end gap-10 max-sm:gap-4 w-[30%] h-[28vw] max-sm:h-2/5 max-sm:w-full sm:pt-4 pb-2 "
+      className=" flex-between flex-col max-sm:justify-end gap-4 sm:gap-10 w-full sm:w-[30%] h-2/5 sm:h-[28vw] sm:pt-4 pb-2"
       initial={{ opacity: 0, scale: 0.7 }}
       animate={{ opacity: 1, scale: 1 }}
-      transition={{ ease: 'easeInOut', duration: 1, delay: 0.4 }}
+      transition={{ ease: 'easeInOut', duration: 1, delay: 0.6 }}
     >
+      {/* Image with Purple Filter */}
       <div
-        className=" rounded-full h-[17vw] w-[17vw] relative -z-10 
-          max-tablet:h-40 max-tablet:w-40 flex-center 
-        "
+        className=" rounded-full h-40 aspect-square tablet:size-[17vw] relative -z-10 flex-center"
       >
         <motion.div
-          className="bg-purple-400 rounded-full h-5/6 w-5/6
-          blur-[70px] opacity-90
-          "
+          className="bg-purple-400 rounded-full size-5/6 blur-[70px] opacity-90"
           initial={{
             opacity: 0,
             scale: 0,
@@ -41,11 +38,11 @@ function HomePageLeft({heroImage}: {heroImage : any}) {
         />
       </div>
 
-      <div className="w-5/6 max-sm:w-full flex flex-col justify-end items-center gap-2 ">
-        <p className="text-[1.3vw] max-tablet:text-base w-full text-center">
+      <div className="w-full sm:w-5/6 flex flex-col justify-end items-center gap-5 ">
+        <p className="text-base tablet:text-[1.3vw] w-full text-center">
           Hey! 👋, Let&apos;s connect 🚀
         </p>
-        <SocialHandles additionalStyle="flex-center gap-4 max-sm:gap-2 rounded-xl   max-sm:w-40 " />
+        <SocialHandles additionalStyle="flex-center gap-2 sm:gap-4 rounded-xl  max-sm:w-40 " />
       </div>
     </motion.div>
   )
