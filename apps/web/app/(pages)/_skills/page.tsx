72df854fb3472c0469a80fa31982d78ee8ba940c
amartripathi0 <theamartripathi@gmail.com>
2024-08-06 21:10:33 +0530

    fix: skills page

--- apps/web/app/(pages)/_skills/page.tsx
+++ apps/web/app/(pages)/_skills/page.tsx
@@ -1,3 +1,4 @@
+import BackgroundBlur from '@/components/shared/BackgroundBlur'
 import PageTitle from '@/components/shared/PageTitle'
 import SkillsScrollContainer from '@/components/shared/SkillsScrollContainer'
 import { backendSkills, frontendSkills } from '@/constants'
@@ -10,13 +11,13 @@ function Skills() {
       <PageTitle
         pageTitle="Skills & Tools"
         containerDivStyles="max-sm:w-full  max-sm:flex-center  max-sm:mx-0 mx-28"
-        underlineStyles=" bg-gradient-to-r from-indigo-600 via-purple-400 to-yellow-500"
+        underlineStyles=" bg-gradient-to-r from-indigo-600 via-purple-400"
       />
 
       {/* bg gradient purple */}
-      <div
-        className="absolute opacity-80 left-36 h-1/5 top-4 max-sm:top-10 max-sm:left-28 aspect-square blur-[100px] rounded-full bg-gradient-to-r from-indigo-600 via-yellow-300 to-purple-400    
-      "
+      <BackgroundBlur
+      blurColor='purple'
+      style = "absolute opacity-80 left-36 h-1/5 top-4 max-sm:top-10 max-sm:left-28 aspect-square blur-[100px] rounded-full bg-gradient-to-r from-indigo-600 via-yellow-300 to-purple-400"
       />
 
       <div className="h-full text-sm max-sm:text-sm max-sm:px-5 text-justify max-sm:rounded-3xl w-full overflow-hidden flex items-center sm:gap-20 max-sm:justify-center flex-col relative">
