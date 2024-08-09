d41e6245f276c2fbeb00b965dccc46948553fada
amartripathi0 <theamartripathi@gmail.com>
2024-08-09 11:04:45 +0530

    fix: add responsiveness in skills and tools page

--- apps/web/app/(pages)/_skills/page.tsx
+++ apps/web/app/(pages)/_skills/page.tsx
@@ -6,28 +6,24 @@ function Skills() {
   return (
     <div
       id="skills"
-      className="relative -mx-28 flex h-[calc(100vh-4rem)] flex-col justify-around pt-10 max-sm:mx-0 max-sm:pt-20 sm:gap-14"
+      className="relative flex h-[calc(100vh-4rem)] flex-col items-center gap-4 pt-10 sm:pt-20 md:items-start md:gap-0"
     >
       <PageTitle
         pageTitle="Skills & Tools"
-        containerDivStyles="max-sm:w-full  max-sm:flex-center  max-sm:mx-0 mx-28"
-        underlineStyles=" bg-gradient-to-r from-indigo-600 via-purple-400"
+        underlineStyles="bg-gradient-to-r from-indigo-600 via-purple-400"
       />
 
       {/* bg gradient purple */}
       <BackgroundBlur
         blurColor="purple"
-        style="absolute opacity-80 left-36 h-1/5 top-4 max-sm:top-10 max-sm:left-28 aspect-square blur-[100px] rounded-full bg-gradient-to-r from-indigo-600 via-yellow-300 to-purple-400"
+        style="absolute opacity-80 left-2/5 md:left-16 h-1/5 top-10  aspect-square blur-[100px] rounded-full bg-gradient-to-r from-indigo-600 via-yellow-300 to-purple-400"
       />
 
-      <div className="relative flex h-full w-full flex-col items-center overflow-hidden text-justify text-sm max-sm:justify-center max-sm:rounded-3xl max-sm:px-5 max-sm:text-sm sm:gap-20">
+      <div className="flex-center w-full h-4/5 md:h-full flex-col overflow-hidden">
         <SkillsScrollContainer
-          skillsArray={frontendSkills}
-          slideSide="leftSlide"
-        />
-        <SkillsScrollContainer
-          skillsArray={backendSkills}
-          slideSide="rightSlide"
+          frontendSkillsArray={frontendSkills}
+          backendSkillsArray={backendSkills}
+          // slideSide="leftSlide"
         />
       </div>
     </div>
--- apps/web/components/shared/SkillsScrollContainer.tsx
+++ apps/web/components/shared/SkillsScrollContainer.tsx
@@ -5,44 +5,48 @@ import { useRef } from 'react'
 import Skill from '../Skill'
 
 interface SkillsScrollContainerProps {
-  skillsArray: SkillType[]
-  slideSide: 'leftSlide' | 'rightSlide'
+  frontendSkillsArray: SkillType[]
+  backendSkillsArray: SkillType[]
+  // slideSide: 'leftSlide' | 'rightSlide'
 }
 function SkillsScrollContainer({
-  skillsArray,
-  slideSide,
+  frontendSkillsArray,
+  backendSkillsArray,
+  // slideSide,
 }: SkillsScrollContainerProps) {
   const targetRef = useRef(null)
 
   const { scrollYProgress } = useScroll({
     target: targetRef,
-    offset: ['start 0.7', 'start end'],
+    offset: ['start 0.9', 'start end'],
   })
 
-  const leftSlide = useTransform(scrollYProgress, [0, 10], ['0%', '-100vw'])
-  const rightSlide = useTransform(scrollYProgress, [0, 10], ['0%', '100vw'])
+  const leftSlide = useTransform(scrollYProgress, [0, 1], ['0%', '-15vw'])
+  const rightSlide = useTransform(scrollYProgress, [0, 1], ['0%', '15vw'])
   const opacity = useTransform(scrollYProgress, [0.3, 1], [1, 0])
 
-  return slideSide === 'leftSlide' ? (
-    <motion.div
-      ref={targetRef}
-      style={{ translateX: leftSlide, opacity: opacity }}
-      className="flex flex-wrap gap-4 px-10 py-6 max-sm:justify-around max-sm:p-2"
-    >
-      {skillsArray.map(({ name, imageSrc }) => (
-        <Skill key={name} name={name} imageSrc={imageSrc} />
-      ))}
-    </motion.div>
-  ) : (
-    <motion.div
-      ref={targetRef}
-      style={{ translateX: rightSlide, opacity: opacity }}
-      className="flex flex-wrap gap-4 px-10 max-sm:justify-around max-sm:p-2"
-    >
-      {skillsArray.map(({ name, imageSrc }) => (
-        <Skill key={name} name={name} imageSrc={imageSrc} />
-      ))}
-    </motion.div>
+  return (
+    <>
+      <motion.div
+        ref={targetRef}
+        style={{ translateX: leftSlide, opacity: opacity }}
+        className="flex flex-wrap justify-center gap-4 md:py-6 p-2 lg:py-10 "
+      >
+        {frontendSkillsArray.map(({ name, imageSrc }) => (
+          <Skill key={name} name={name} imageSrc={imageSrc} />
+        ))}
+      </motion.div>
+
+      <motion.div
+        ref={targetRef}
+        style={{ translateX: rightSlide, opacity: opacity }}
+        className="flex flex-wrap justify-center gap-4 md:pb-12 max-sm:p-2"
+      >
+        {backendSkillsArray.map(({ name, imageSrc }) => (
+          <Skill key={name} name={name} imageSrc={imageSrc} />
+        ))}
+      </motion.div>
+    </>
   )
 }
 
