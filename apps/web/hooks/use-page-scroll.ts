b88e49a536e314ae1496e03e1970e7a5f8494eb5
amartripathi0 <theamartripathi@gmail.com>
2024-08-09 22:46:32 +0530

    fix: optimize use-page-scroll hook

--- apps/web/hooks/use-page-scroll.ts
+++ apps/web/hooks/use-page-scroll.ts
@@ -1,76 +1,64 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
+
 function usePageScroll(threshold = 10) {
   const [pageSectionOnViewport, setPageSectionOnViewport] = useState('home')
-  const [windowHeight, setWindowHeight] = useState(0)
   const [isScrolling, setIsScrolling] = useState(false)
   const [scrolled, setScrolled] = useState(false)
 
-  useEffect(() => {
+  const handleScrollClass = useCallback(() => {
     if (isScrolling) {
-      if (!document.body.classList.contains('on-scroll')) {
-        document.body.classList.add('on-scroll')
-        document.body.classList.remove('on-not-scroll')
-      }
+      document.body.classList.add('on-scroll')
+      document.body.classList.remove('on-not-scroll')
     } else {
-      if (!document.body.classList.contains('on-not-scroll')) {
-        document.body.classList.add('on-not-scroll')
-        document.body.classList.remove('on-scroll')
-      }
+      document.body.classList.add('on-not-scroll')
+      document.body.classList.remove('on-scroll')
     }
   }, [isScrolling])
+
   useEffect(() => {
-    setWindowHeight(window.innerHeight)
-    function handlePageScroll() {
+    handleScrollClass()
+  }, [isScrolling, handleScrollClass])
+
+  useEffect(() => {
+    const windowHeight = window.innerHeight
+    let scrollTimer: NodeJS.Timeout
+
+    // Function to determine which section is currently in view
+    const handlePageNavigation = () => {
+      const scrollY = window.scrollY
+      const sections = [
+        { name: 'home', threshold: 0.8 },
+        { name: 'about-me', threshold: 1.6 },
+        { name: 'skills', threshold: 2.4 },
+        { name: 'projects', threshold: 4.4 },
+        { name: 'contact-me', threshold: Infinity },
+      ]
+
+      const currentSection = sections.find(
+        (section) => scrollY < section.threshold * windowHeight,
+      )
+      if (currentSection) setPageSectionOnViewport(currentSection.name)
+    }
+
+    const handleScroll = () => {
+      setScrolled(window.scrollY > threshold)
+      setIsScrolling(true)
+      clearTimeout(scrollTimer)
+      scrollTimer = setTimeout(() => setIsScrolling(false), 100)
+    }
+
+    const handlePageScroll = () => {
       handlePageNavigation()
-      handleScrollbar()
       handleScroll()
     }
 
-    function handleScrollbar() {
-      handleScrolling()
-    }
-    let scrollTimer: NodeJS.Timeout
-    function handleScrolling() {
-      setIsScrolling(true)
-      clearInterval(scrollTimer)
-      scrollTimer = setTimeout(() => setIsScrolling(false), 100)
-    }
-    function handlePageNavigation() {
-      if (window.scrollY <= windowHeight * 0.8) {
-        setPageSectionOnViewport('home')
-      } else if (
-        window.scrollY >= windowHeight * 0.8 &&
-        window.scrollY < windowHeight * 1.6
-      ) {
-        setPageSectionOnViewport('about-me')
-      } else if (
-        window.scrollY >= windowHeight * 1.6 &&
-        window.scrollY < 2.4 * windowHeight
-      ) {
-        setPageSectionOnViewport('skills')
-      } else if (
-        window.scrollY >= 2.4 * windowHeight &&
-        window.scrollY < 4.4 * windowHeight
-      ) {
-        setPageSectionOnViewport('projects')
-      } else if (window.scrollY > 4.4 * windowHeight) {
-        setPageSectionOnViewport('contact-me')
-      }
-    }
-    function handleScroll() {
-      if (window.scrollY > threshold) {
-        setScrolled(true)
-      } else {
-        setScrolled(false)
-      }
-    }
     window.addEventListener('scroll', handlePageScroll)
     return () => {
       window.removeEventListener('scroll', handlePageScroll)
       clearTimeout(scrollTimer)
     }
-  }, [windowHeight, threshold])
+  }, [threshold])
 
   return { pageSectionOnViewport, scrolled }
 }
