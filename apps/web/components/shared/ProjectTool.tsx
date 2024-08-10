3351bdddd24d3465feac2c8cc78783d65ea38942
amartripathi0 <theamartripathi@gmail.com>
2024-08-10 15:31:38 +0530

    fix: add explicit font montserrat in project tool badge

--- apps/web/components/shared/ProjectTool.tsx
+++ apps/web/components/shared/ProjectTool.tsx
@@ -6,7 +6,7 @@ import { Badge } from '../ui/badge'
 function ProjectTool({ tool }: { tool: string }) {
   return (
     <motion.div key={tool} whileHover={{ scale: 1.06, rotate: -5 }}>
-      <Badge className="bg-prelude-900 px-2 py-1 max-sm:text-[3vw] sm:p-2 sm:px-4">
+      <Badge className="bg-prelude-900 px-2 py-1 max-sm:text-[3vw] sm:p-2 sm:px-4 font-montserrat">
         <TextUnderline text={tool} textStyles="text-xs text-zinc-200" />
       </Badge>
     </motion.div>
