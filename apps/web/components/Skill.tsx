ae4bf6c674798281d020b3fb7ed8da1ded9b0f02
amartripathi0 <theamartripathi@gmail.com>
2024-09-06 23:30:58 +0530

    fix: skill name on skill&tool hover

--- apps/web/components/Skill.tsx
+++ apps/web/components/Skill.tsx
@@ -5,20 +5,23 @@ import { BackgroundGradient } from './ui/BackgroundGradient'
 
 function Skill({ name, imageSrc }: { name: string; imageSrc: string }) {
   return (
-    <BackgroundGradient>
+    <BackgroundGradient className="custom-cursor-hidden">
       {/* <div className="max-sm:flex-center flex-between aspect-square md:h-24  lg:h-32 flex-col rounded-3xl bg-zinc-800 p-2 max-sm:h-16 max-sm:p-2"> */}
-      <div className="flex-center aspect-square h-16 flex-col rounded-3xl bg-zinc-800 p-2 max-sm:p-2 md:h-16 lg:h-24">
+      <div className="custom-cursor-hidden flex-center group relative aspect-square h-16 flex-col rounded-3xl bg-zinc-800 p-2 hover:opacity-90 max-sm:p-2 md:h-16 lg:h-24">
         <Image
           src={imageSrc}
           alt={name}
           width={200}
           height={200}
           // className="aspect square h-2/3 rounded-2xl object-contain max-lg:h-full"
-          className="aspect square rounded-2xl object-contain"
+          className="custom-cursor-hidden aspect square rounded-2xl object-contain"
         />
         {/* <Badge className="flex-center h-6 w-full bg-prelude-900 max-lg:hidden">
           <TextUnderline text={name} textType="skill" />
         </Badge> */}
+        <div className="custom-cursor-hidden invisible absolute w-4/5 rounded-xl bg-prelude-900 p-2 text-center text-[11px] font-bold max-sm:text-[3vw] md:group-hover:visible">
+          {name}
+        </div>
       </div>
     </BackgroundGradient>
   )
