c75f81c3871e0c8a97fa22010903aec127832378
amartripathi0 <theamartripathi@gmail.com>
2024-09-21 10:07:04 +0530

    fix: remove scroll thumb color change on scroll

--- apps/web/hooks/use-page-scroll.ts
+++ apps/web/hooks/use-page-scroll.ts
@@ -3,22 +3,22 @@ import { useEffect, useState, useCallback } from 'react'
 
 function usePageScroll(threshold = 10) {
   const [pageSectionOnViewport, setPageSectionOnViewport] = useState('home')
-  const [isScrolling, setIsScrolling] = useState(false)
+  // const [isScrolling, setIsScrolling] = useState(false)
   const [scrolled, setScrolled] = useState(false)
 
-  const handleScrollClass = useCallback(() => {
-    if (isScrolling) {
-      document.body.classList.add('on-scroll')
-      document.body.classList.remove('on-not-scroll')
-    } else {
-      document.body.classList.add('on-not-scroll')
-      document.body.classList.remove('on-scroll')
-    }
-  }, [isScrolling])
+  // const handleScrollClass = useCallback(() => {
+  //   if (isScrolling) {
+  //     document.body.classList.add('on-scroll')
+  //     document.body.classList.remove('on-not-scroll')
+  //   } else {
+  //     document.body.classList.add('on-not-scroll')
+  //     document.body.classList.remove('on-scroll')
+  //   }
+  // }, [isScrolling])
 
-  useEffect(() => {
-    handleScrollClass()
-  }, [isScrolling, handleScrollClass])
+  // useEffect(() => {
+  //   // handleScrollClass()
+  // }, [isScrolling, handleScrollClass])
 
   useEffect(() => {
     const windowHeight = window.innerHeight
@@ -29,9 +29,9 @@ function usePageScroll(threshold = 10) {
       const scrollY = window.scrollY
       const sections = [
         { name: 'home', threshold: 0.8 },
-        { name: 'about-me', threshold: 1.6 },
-        { name: 'skills', threshold: 2.4 },
-        { name: 'projects', threshold: 4.4 },
+        { name: 'work-exp', threshold: 1.4 },
+        { name: 'projects', threshold: 2.4 },
+        { name: 'skills', threshold: 4 },
         { name: 'contact-me', threshold: Infinity },
       ]
 
@@ -43,9 +43,9 @@ function usePageScroll(threshold = 10) {
 
     const handleScroll = () => {
       setScrolled(window.scrollY > threshold)
-      setIsScrolling(true)
+      // setIsScrolling(true)
       clearTimeout(scrollTimer)
-      scrollTimer = setTimeout(() => setIsScrolling(false), 100)
+      // scrollTimer = setTimeout(() => setIsScrolling(false), 100)
     }
 
     const handlePageScroll = () => {
