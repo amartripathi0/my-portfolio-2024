0497e02ac8e8d9b36b7663b1eba7e769e11b2e5e
amartripathi0 <theamartripathi@gmail.com>
2024-11-11 23:07:13 +0530

    fix: next config remotePatterns

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["cdn.sanity.io", "via.placeholder.com" , 'roboto.studio/logo.svg', 'media.licdn.com'],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'roboto.studio',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+      },
+    ],
+  },
 }
 
 export default nextConfig
