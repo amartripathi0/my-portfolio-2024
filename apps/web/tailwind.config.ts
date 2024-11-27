2a86709c551d39b6e0e9a536b28f5a5aa3f5b662
amartripathi0 <theamartripathi@gmail.com>
2024-11-27 13:52:42 +0530

    fix: block css issue

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -11,6 +11,7 @@ const config = {
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
+    './blocks/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
